4d46ba493fcaf1f43cf30f482a15905738adf4d7
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-16 16:43:29 +0800

    DRF introduction

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Movie
+from django.http import JsonResponse
 
-# Create your views here.
+
+def movie_list(request):
+    # retrieve queryset (complex datatype)
+    movies_queryset = Movie.objects.all()
+
+    # dictionary -> list -> dictionary
+    data = {
+        'movies': list(movies_queryset.values())
+    }
+
+    # dictionary -> json
+    return JsonResponse(data)
+
+
+def move_detail(request, pk):
+    movie_queryset = Movie.objects.get(pk=pk)
+
+    data = {
+        'name': movie_queryset.name,
+        'description': movie_queryset.description,
+        'active': movie_queryset.active
+    }
+
+    return JsonResponse(data)
