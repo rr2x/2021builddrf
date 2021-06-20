00d51345b943c01b905ffd8ebc8ce17ca821aaf5
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-20 19:54:30 +0800

    link user per review, basic api login using built-in (temporary for testing purposes)

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status, generics, viewsets
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from watchlist_app.models import WatchList, StreamPlatform, Review
 from .serializers import ReviewSerializer, StreamPlatformSerializer2, WatchListSerializer, StreamPlatformSerializer
@@ -10,11 +11,22 @@ from .serializers import ReviewSerializer, StreamPlatformSerializer2, WatchListS
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     # override create method
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         movie = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=movie)
+
+        review_user = self.request.user  # access to logged in 'user' in request
+        review_queryset = Review.objects.filter(
+            watchlist=movie, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie")
+
+        serializer.save(watchlist=movie, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
