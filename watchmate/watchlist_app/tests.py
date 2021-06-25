30f5e4e3bc8cb44a3584f28f0c825690b6a1d50c
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-25 20:50:15 +0800

    final commit

--- watchmate/watchlist_app/tests.py
+++ watchmate/watchlist_app/tests.py
@@ -156,3 +156,8 @@ class ReviewTestCase(APITestCase):
         response = self.client.get(
             reverse('review-detail', args=(self.review.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_review_user(self):
+        response = self.client.get(
+            '/watch/reviews/?username=' + self.user.username)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
