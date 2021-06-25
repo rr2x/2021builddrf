eeeac95530a12f1d2750efa2a8dfadfee76ec0d2
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-25 17:09:36 +0800

    testing for: registration, login, logout

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -43,4 +43,4 @@ def registration_view(request):
         else:
             data = serializer.errors
 
-        return Response(data)
+        return Response(data, status=status.HTTP_201_CREATED)
--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            "username": "testcase",
+            "email": "testcase@example.com",
+            "password": "newpwd123#@!",
+            "password2": "newpwd123#@!"
+        }
+
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="example",
+            email="e@e.com",
+            password="newPwd123#@!")
+
+    def test_login(self):
+        data = {
+            "username": "example",
+            "email": "e@e.com",
+            "password": "newPwd123#@!",
+        }
+
+        response = self.client.post(reverse('login'), data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username='example')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
